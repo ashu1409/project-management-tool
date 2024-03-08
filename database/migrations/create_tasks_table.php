3d488e6c80a3e68cf467ba3584423b7d3efd87fb
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-03-08 15:17:22 +0530

    Update create_tasks_table.php

--- database/migrations/create_tasks_table.php
+++ database/migrations/create_tasks_table.php
@@ -1,1 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('status', ['completed', 'pending'])->default('pending');
+            $table->string('attachment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
 
