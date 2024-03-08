ea6dcc77777b448e4d126d3f7942165f438e7427
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-03-08 15:32:31 +0530

    Create TaskDelete.vue

---
+++ TaskDelete.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Are you sure you want to delete this task?</p>
+    <button @click="confirmDelete">Yes</button>
+    <button @click="cancelDelete">No</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    confirmDelete() {
+      // Emit an event to parent component to confirm deletion
+      this.$emit('confirm-delete');
+    },
+    cancelDelete() {
+      // Emit an event to parent component to cancel deletion
+      this.$emit('cancel-delete');
+    }
+  }
+};
+</script>
+
+<style>
+/* Add your component styles here */
+</style>
